176e428e7f9e17386e0bc8ceb591399c3a76b399
7crescentmoon <mirfan1537@gmail.com>
2025-05-25 19:42:21 +0700

    update message response add cart

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,7 +1,7 @@
 // controllers/cartController.js
 const axios = require("axios");
-const { getCart, create , remove } = require("../model/cart");
-require('dotenv').config();
+const { getCart, create, remove } = require("../model/cart");
+require("dotenv").config();
 // URL endpoint product-service (misalnya)
 const PRODUCT_SERVICE_URL = process.env.PRODUCT_SERVICE_URL;
 
@@ -17,10 +17,15 @@ async function getCartAll(req, res) {
     const productDetails = await Promise.all(
       productIds.map(async (productId) => {
         const productResponse = await axios.get(
-          `${PRODUCT_SERVICE_URL}${productId}`,{
+          `${PRODUCT_SERVICE_URL}${productId}`,
+          {
             headers: {
-              Authorization: `Bearer ${req.headers['authorization']?.split(' ')[1]}`  // Token yang dikirim dari frontend
-            }});
+              Authorization: `Bearer ${
+                req.headers["authorization"]?.split(" ")[1]
+              }`, // Token yang dikirim dari frontend
+            },
+          }
+        );
         return productResponse.data; // Asumsi data produk ada di `data` response
       })
     );
@@ -37,14 +42,14 @@ async function getCartAll(req, res) {
       items: cartWithProductDetails,
     });
   } catch (error) {
-    res.status(500).send({message: error.message});
+    res.status(500).send({ message: error.message });
   }
 }
 
 // Endpoint untuk menambahkan item ke dalam cart
 async function addToCart(req, res) {
-   console.log(req.body);
-  
+  console.log(req.body);
+
   try {
     const userId = req.user.userId; // Mengambil userId dari token yang sudah didekode
     const itemId = req.body.productId; // Mengambil ID item dari body request
@@ -52,13 +57,14 @@ async function addToCart(req, res) {
     // Mendapatkan detail produk berdasarkan ID produk dari product-service
     const productResponse = await axios.get(`${PRODUCT_SERVICE_URL}${itemId}`, {
       headers: {
-        Authorization: `Bearer ${req.headers['authorization']?.split(' ')[1]}`  // Token yang dikirim dari frontend
-      }});
+        Authorization: `Bearer ${req.headers["authorization"]?.split(" ")[1]}`, // Token yang dikirim dari frontend
+      },
+    });
     const product = productResponse.data;
-    
+
     // Jika produk tidak ditemukan
     if (!product) {
-      return res.status(404).send('Product not found');
+      return res.status(404).send("Product not found");
     }
 
     // Menambahkan produk ke cart
@@ -68,10 +74,13 @@ async function addToCart(req, res) {
       price: product.price,
       quantity: req.body.quantity || 1, // Menentukan jumlah produk, default 1
     };
-    
+
     // Memasukkan item ke dalam cart
     await create(userId, item); // Memperbaiki pemanggilan create yang benar
-    res.status(200).send('Item added to cart');
+    res.status(200).send({
+      message: "Item added to cart",
+      cartId: cartId,
+    });
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -79,17 +88,17 @@ async function addToCart(req, res) {
 
 async function removeFromCart(req, res) {
   console.log(req.params);
-  
+
   try {
     const userId = req.user.userId;
     const itemId = req.params.itemId; // ID produk yang ingin dihapus
 
     // Menghapus produk dari cart berdasarkan itemId
     await remove(userId, itemId);
-    res.status(200).send('Item removed from cart');
+    res.status(200).send("Item removed from cart");
   } catch (error) {
     res.status(500).send(error.message);
   }
 }
 
-module.exports = { getCartAll , addToCart, removeFromCart };
+module.exports = { getCartAll, addToCart, removeFromCart };
